95217c866ae09b390343a96f917a556b3030a118
Sgoldik <sgoldik@mail.ru>
2019-08-08 18:06:35 +0700

    Add left side menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,55 +6,186 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import clsx from 'clsx';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import { mainListItems, secondaryListItems } from './listItems';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
     display: 'flex',
   },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    backgroundColor: "#654EA3"
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
   },
   title: {
     flexGrow: 1,
   },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    //height: '100%'
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+    //height: '100%'
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
 }));
+
+const drawerWidth = 240;
 
 export default function NavBar(props) {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   if (props.address != "Login") {
     return (
       <div className={classes.root}>
-        <AppBar position={props.position}>
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" className={classes.title}>
+            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
               deLance
             </Typography>
-            <Typography variant="h6">
-              {props.address}, balance: {props.balance}
+            <Typography component="h1" variant="h6" color="inherit">
+              {props.address}, balance {props.balance}
             </Typography>
           </Toolbar>
         </AppBar>
-      </div>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+    </div>
     );
   } else {
     return (
       <div className={classes.root}>
-        <AppBar position={props.position}>
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" className={classes.title}>
+            <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
               deLance
             </Typography>
             <Button color="inherit" onClick={() => { props.auth() }}>{props.address}</Button>
           </Toolbar>
         </AppBar>
-      </div>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>{mainListItems}</List>
+        <Divider />
+        <List>{secondaryListItems}</List>
+      </Drawer>
+    </div>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,137 @@
 import ReactDOM from "react-dom";
 import React from 'react';
 import NavBar from './components/navbar'
-import Description from './components/description'
 const nodeUrl = 'https://testnodes.wavesnodes.com';
-const seed = 'melody eye stock ostrich camera talk unlock royal insane pipe step squeeze';
-const address = '3N67wqt9Xvvn1Qtgz6KvyEcdmr8AL7EVaQM';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import * as nodeInt from './modules/nodeInt';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import clsx from 'clsx';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 240,
+  },
+}));
+
+export default function Main (props) {
+	const classes = useStyles();
+	const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+	return (
+		<div className={classes.root}>
+		<CssBaseline />
+		<NavBar auth={props.auth} address={props.address ? props.address : "Login"} balance={props.balance ? props.balance : "Nan" }/>
+		<main className={classes.content}>
+			<div className={classes.appBarSpacer} />
+			<Container maxWidth="lg" className={classes.container}>
+				<Grid container spacing={3}>
+					{/* Chart */}
+					<Grid item xs={12} md={8} lg={9}>
+						<Paper className={fixedHeightPaper}>
+						 
+						</Paper>
+					</Grid>
+					{/* Recent Deposits */}
+					<Grid item xs={12} md={4} lg={3}>
+						<Paper className={fixedHeightPaper}>
+						 
+						</Paper>
+					</Grid>
+					{/* Recent Orders */}
+					<Grid item xs={12}>
+						<Paper className={classes.paper}>
+							
+						</Paper>
+					</Grid>
+				</Grid>
+			</Container>
+			<MadeWithLove />
+		</main>
+	</div>
+	)
+}
 
 class App extends React.Component {
+	
   constructor(props) {
    super(props);
     this.state = {
@@ -40,20 +163,25 @@ class App extends React.Component {
       alert("To Auth WavesKeeper should be installed.");
     }
 	}
-
   render() {
-	  return (
-  	  <div className="container" style={{display: 'flex', flexDirection: "column", padding: 0, margin: 0}}>
-        <NavBar position="static" auth={this.authFunc} address={this.state.keeperData.address ? this.state.keeperData.address : "Login"} balance={this.state.balance ? this.state.balance : "Nan" } />
-        <Container maxWidth="lg">
-          <Description />
-        </Container>
-      </div>
+		return (
+	  	<Main auth={this.authFunc} address={this.state.keeperData.address ? this.state.keeperData.address : "Login"} balance={this.state.balance ? this.state.balance : "Nan" }/>
     )
   }
 }
 
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <Link color="inherit" href="https://netsy.de/">
+        NetSyde
+      </Link>
+      {' team.'}
+    </Typography>
+  );
+}
 const app = document.getElementById('app');
 if (app) {
-  ReactDOM.render(<App/>, app);
+  ReactDOM.render(<App />, app);
 }
