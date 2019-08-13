fec6370bf3adea463d4656db349f7ec0128ea4ac
Sgoldik <sgoldik@mail.ru>
2019-08-13 13:22:59 +0700

    0.0.6

--- src/modules/nodeInt.js
+++ src/modules/nodeInt.js
@@ -1,9 +1,25 @@
-const {nodeInteraction} =  require('@waves/waves-transactions');
+const {broadcast, data} =  require('@waves/waves-transactions');
 const axios = require('axios');
+
 export let getBalance = async (address, nodeUrl) => {
     try {
         return await axios.get(`${nodeUrl}/addresses/balance/${address}`).then(resp => {return resp.data});
     } catch (e) {
-        console.log(e)
+        console.log(`ERROR in nodeInt.getBalance! ${e.name}: ${e.message}\n${e.stack}`);
+    }
+}
+
+export let dataTx = async (info, seed, nodeUrl) => {
+    try {
+        broadcast( data( {data: info, fee: 100000}, seed), nodeUrl ).then( (v) => {
+            if (v.id) {
+                console.log(`Data transaction id: ${v.id}`);
+                return true;
+            } else {
+                return false;
+            }
+        })
+    } catch (e) {
+        console.log(`ERROR in nodeInt.dataTx! ${e.name}: ${e.message}\n${e.stack}`);
     }
 }
