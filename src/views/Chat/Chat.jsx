665515e41668f805b1b897353b1cb062c4fe096e
Sgoldik <sgoldik@mail.ru>
2019-11-24 14:52:31 +0700

    0.3.1.2

--- src/views/Chat/Chat.jsx
+++ src/views/Chat/Chat.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import moment from 'moment';
-
+import { observer, inject } from 'mobx-react';
 import useRouter from '../../utils/useRouter';
 import { Page } from '../../components';
 import {
@@ -11,6 +11,7 @@ import {
   ConversationPlaceholder
 } from './components';
 const uuid = require('uuid/v4');
+import Error401 from '../Error401'
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100%',
@@ -50,7 +51,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Chat = () => {
+const Chat = inject('rootStore')(observer(({ rootStore }) => {
   const classes = useStyles(1);
   const router = useRouter();
   const [conversations, setConversations] = useState([]);
@@ -205,29 +206,34 @@ const Chat = () => {
       c => c.id === router.match.params.id
     );
   }
-
-  return (
-    <Page
-      className={clsx({
-        [classes.root]: true,
-        [classes.openConversion]: selectedConversation
-      })}
-      title="Chat"
-    >
-      <ConversationList
-        className={classes.conversationList}
-        conversations={conversations}
-      />
-      {selectedConversation ? (
-        <ConversationDetails
-          className={classes.conversationDetails}
-          conversation={selectedConversation}
+  if (rootStore.user.isUserLogin && rootStore.user.isUserReg) {
+    return (
+      <Page
+        className={clsx({
+          [classes.root]: true,
+          [classes.openConversion]: selectedConversation
+        })}
+        title="Chat"
+      >
+        <ConversationList
+          className={classes.conversationList}
+          conversations={conversations}
         />
-      ) : (
-        <ConversationPlaceholder className={classes.conversationPlaceholder} />
-      )}
-    </Page>
-  );
-};
+        {selectedConversation ? (
+          <ConversationDetails
+            className={classes.conversationDetails}
+            conversation={selectedConversation}
+          />
+        ) : (
+          <ConversationPlaceholder className={classes.conversationPlaceholder} />
+        )}
+      </Page>
+    );
+  } else {
+    return (
+      <Error401 />
+    )
+  }
+}));
 
 export default Chat;
